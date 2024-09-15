c3c9afd315141d5cabe0e84c3a3eb4627e316fba
Prasad790-sys <psa19974@gmail.com>
2024-09-15 17:31:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function indexOfIgnoreCase(s1, s2) {
    let st1=s1.toLowerCase();
    let st2=s2.toLowerCase();
 	console.log(st1+" "+st2);
-   for(let i=0;i<st1.length;i++){
+   for(let i=0 i<st1.length i++){
 	   if(st1[i]==st2[0]&i+st2.length<st1.length){
 		   let s3=st1.slice(i,i+st2.length+1);
-		   if(s3===st2)){ans=i;}
+		   if(s3===st2){ans=i;}
 		   break;
 	   }
    }
