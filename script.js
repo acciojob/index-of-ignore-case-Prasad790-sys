3b11edbb1c50526889b2a5ff588f9ea890d68754
Prasad790-sys <psa19974@gmail.com>
2024-09-15 18:13:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function indexOfIgnoreCase(s1, s2) {
   let st1=s1.toLowerCase();
   let st2=s2.toLowerCase();
   for (let index = 0; index < s1.length-s2.length; index++) {
-  	if(st1.includes(st2, index)){ans=index+1;}
+  	if(st1.includes(st2, index)){ans=index;}
   }
 	return ans;
 }
-// Please do not change the code b
+// Please do not change the code
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
